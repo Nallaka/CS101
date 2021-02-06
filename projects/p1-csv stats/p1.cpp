6f98d1ed7b113e30e7fe2827bdfd9ac8e0852655
nallaka <mithul0700@gmail.com>
2021-02-06 16:34:00 -0600

    Min, Max, Avg Functions written Invalid input only implemented for avg

--- projects/p1-csv stats/p1.cpp
+++ projects/p1-csv stats/p1.cpp
@@ -6,6 +6,7 @@
 #include <sstream>
 #include <fstream>
 #include <iomanip>
+#include <math.h>
 
 using namespace std;
 
@@ -28,7 +29,7 @@ int main(int argc, char* argv[]) {
     std::ifstream ifstream1(path);
 
     if (!ifstream1.is_open()) {
-       cout << "Failed to open " << path << endl;
+       cout << "Failed to open \"" << path << "\"" << endl;
        return 0;
     }
 
@@ -45,13 +46,15 @@ int main(int argc, char* argv[]) {
     }
 
     string line;
-    int entries = 0;
+    int entries = 1;
     while (getline(ifstream1, line)) {
-        stringstream string_stream(line);
-        entries++;
+        if (!line.empty()) {
+            entries++;
+        }
     }
 
-    string data[entries+1][columns];
+    //Data Array
+    string data[entries][columns];
 
     ifstream1.clear();
     ifstream1.seekg(0,std::ios::beg);
@@ -68,6 +71,7 @@ int main(int argc, char* argv[]) {
         i++;
     }
 
+    //Assign Fields
     string temp;
     getline(ifstream1, temp);
 
@@ -83,5 +87,153 @@ int main(int argc, char* argv[]) {
             field++;
         }
         entry++;
+    }
+
+    //Commands
+    bool quit = false;
+
+    while (!quit) {
+        cout << "Enter a command or \"help\" for a command list:" << endl;
+
+        string commandLine;
+        getline(cin, commandLine);
+        stringstream commandStream(commandLine);
+
+        string command;
+        commandStream >> command;
+
+        //quit
+        if (command == "quit") {
+            quit = true;
+        }
+
+        //help
+        if (command == "help") {
+            cout << "command list:\n"
+                    "        print\n"
+                    "        cols\n"
+                    "        search *|col_name val\n"
+                    "        min col_name\n"
+                    "        max col_name\n"
+                    "        avg col_name" << endl;
+            cout << endl;
+            continue;
+        }
+
+        //cols
+        if (command == "cols") {
+            for (int c = 0; c < columns; c++) {
+                cout << data[0][c] << endl;
+            }
+            cout << endl;
+            continue;
+        }
+
+        //print
+        if (command == "print") {
+            for (int e = 0; e < entries; e++) {
+                for (int c = 0; c < columns; c++) {
+                    cout << setw(colWidth) << data[e][c];
+                }
+                cout << endl;
+            }
+            cout << endl;
+            continue;
+        }
+
+        //min
+        if (command == "min") {
+            string col;
+            double min = 1.79769e+308;
+            int minC = 0;
+            int minE = 0;
+            string out = "N/A";
+            commandStream >> quoted(col);
+            for (int c = 0; c < columns; c++) {
+                if (data[0][c] == col) {
+                    for (int e = 1; e < entries; e++) {
+                        if (!data[e][c].empty()) {
+                            if (stod(data[e][c]) < min) {
+                                min = stod(data[e][c]);
+                                minC = c;
+                                minE = e;
+                            }
+                        }
+                    }
+                }
+            }
+
+            if (minE != 0) {
+                out = data[minE][minC];
+            }
+            cout << "The min for col \"" << col << "\" = " << out << endl << endl;
+            continue;
+        }
+
+        //max
+        if (command == "max") {
+            string col;
+            double max = 2.22507e-308;
+            int maxC = 0;
+            int maxE = 0;
+            string out = "N/A";
+            commandStream >> quoted(col);
+            for (int c = 0; c < columns; c++) {
+                if (data[0][c] == col) {
+                    for (int e = 1; e < entries; e++) {
+                        if (!data[e][c].empty()) {
+                            if (stod(data[e][c]) > max) {
+                                max = stod(data[e][c]);
+                                maxC = c;
+                                maxE = e;
+                            }
+                        }
+                    }
+                }
+            }
+
+            if (maxE != 0) {
+                out = data[maxE][maxC];
+            }
+
+            cout << "The max for col \"" << col << "\" = " << out << endl << endl;
+            continue;
+        }
+
+        //avg
+        if (command == "avg") {
+            string col;
+            double total = 0;
+            double count = 0;
+            string out = "N/A";
+            bool colFound = false;
+            commandStream >> quoted(col);
+            for (int c = 0; c < columns; c++) {
+                if (data[0][c] == col) {
+                    colFound = true;
+                    for (int e = 1; e < entries; e++) {
+                        if (!data[e][c].empty()) {
+                            total += stod(data[e][c]);
+                            count++;
+                        }
+                    }
+                }
+            }
+
+            if (colFound) {
+                if (!isnan(total / count)) {
+                    cout << "The avg for col \"" << col << "\" = " << total / count << endl << endl;
+                    continue;
+                } else {
+                    cout << "The avg for col \"" << col << "\" = N/A" << endl << endl;
+                    continue;
+                }
+            } else {
+                cout << "Invalid column \"" << col << "\"" << endl << endl;
+                continue;
+            }
+        }
+
+        cout << "Invalid command" << endl << endl;
     }
 }
